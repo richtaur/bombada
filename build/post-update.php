c9ce7a242cc4bc53bbb945a53acea56be8692bfe
Matt Hackett <matth@paletteswap.com>
2010-01-19 20:07:04 -0800

    getting build stuff almost to readiness zomg

--- build/post-update.php
+++ build/post-update.php
@@ -2,9 +2,9 @@
 <?php
 
 echo "Adding postupdate.txt ...\n";
-file_put_contents('../postupdate.txt', "this would have had to have come from the post-update script\n");
+file_put_contents('postupdate.txt', "this would have had to have come from the post-update script\n");
 
 echo "Creating JS file ...\n";
-file_put_contents('../public/js/postupdate.js', "i guess it's possible!\n");
+file_put_contents('public/js/postupdate.js', "i guess it's possible!\n");
 
 echo "[complete]\n";
