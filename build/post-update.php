90f1e4f1e8243560a5e3471ac65699ffec2dc7d2
Matt Hackett <matth@paletteswap.com>
2010-01-19 20:09:49 -0800

    post-update tweaks

--- build/post-update.php
+++ build/post-update.php
@@ -1,10 +1,10 @@
 #!/usr/bin/php
 <?php
 
-echo "Adding postupdate.txt ...\n";
+echo "Creating postupdate.txt ...\n";
 file_put_contents('postupdate.txt', "this would have had to have come from the post-update script\n");
 
-echo "Creating JS file ...\n";
+echo "Creating js/postupdate.js ...\n";
 file_put_contents('public/js/postupdate.js', "i guess it's possible!\n");
 
 echo "[complete]\n";
